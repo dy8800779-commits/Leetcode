d8bded131c97fd60076ccb2f565fa09843de370e
Dipesh Yadav <dy8800779@gmail.com>
2026-09-17 16:37:43 +0530

    Time: 0 ms (100%), Space: 25.9 MB (76.36%) - LeetHub

--- 2057-smallest-index-with-equal-value/2057-smallest-index-with-equal-value.cpp
+++ 2057-smallest-index-with-equal-value/2057-smallest-index-with-equal-value.cpp
@@ -2,9 +2,6 @@ class Solution {
 public:
     int smallestEqual(vector<int>& nums) {
        int n=nums.size();
-       if(n<0){
-        return -1;
-       } 
        for(int i=0; i<n; i++){
         if(i%10==nums[i]){
             return i;
